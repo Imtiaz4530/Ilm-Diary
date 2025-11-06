f751ca3682ca79558f583d69e8cb5398aa21fd86
Imtiaz4530 <apual4016@gmail.com>
2025-11-07 01:56:39 +0600

    5th commit - laptop - change pagination system - edit feature backend

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -131,31 +131,33 @@ const Home = ({ records }) => {
         )}
       </div>
 
-      <div className={styles.pagination}>
-        <button
-          disabled={currentPage === 1}
-          onClick={() => handlePageChange(currentPage - 1)}
-        >
-          Prev
-        </button>
-
-        {[...Array(totalPages)].map((_, index) => (
+      {totalPages > 1 && (
+        <div className={styles.pagination}>
           <button
-            key={index}
-            className={currentPage === index + 1 ? styles.activePage : ""}
-            onClick={() => handlePageChange(index + 1)}
+            disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
           >
-            {index + 1}
+            Prev
           </button>
-        ))}
 
-        <button
-          disabled={currentPage === totalPages}
-          onClick={() => handlePageChange(currentPage + 1)}
-        >
-          Next
-        </button>
-      </div>
+          {[...Array(totalPages)].map((_, index) => (
+            <button
+              key={index}
+              className={currentPage === index + 1 ? styles.activePage : ""}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 };
